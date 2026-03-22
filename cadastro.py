8386e8b4bd17acfb7783f07871493cada6b6b6c5
Dener <denercardoso76@gmail.com>
2026-03-22 13:55:52 -0300

    adiciona sistema de cadastro

---
+++ cadastro.py
@@ -0,0 +1,23 @@
+while True:
+    print("---- MENU ----")
+    print("1 - Cadastrar")
+    print("2 - Listar")
+    print("3 - Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        nome = input("Nome: ")
+        idade = int(input("Idade: "))
+        with open("cadastros.txt", "a") as arquivo:
+            arquivo.write(f"{nome} - {idade} anos\n")
+        print(f"{nome} cadastrado com sucesso!")
+
+    if opcao == "2":
+        print("---- CADASTROS ----")
+        with open("cadastros.txt", "r") as arquivo:
+            print(arquivo.read())
+
+    if opcao == "3":
+        print("Saindo...")
+        break
